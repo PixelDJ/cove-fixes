498ca4fccc9feaf61acd8c7ba4bd6e85af90b62a
pixel <superdj@gmail.com>
2023-01-11 16:26:27 -0700

    moved services to hash table for readability

--- Restart VSS Services.ps1
+++ Restart VSS Services.ps1
@@ -1,26 +1,33 @@
 function Restart-ServiceWithProgress {
   param(
-    [string]$serviceName,
-    [int]$percentComplete,
-    [string]$status,
-    [bool]$force = $false
+    [string]$Name,
+    [int]$PercentComplete,
+    [string]$Status,
+    [bool]$Force
   )
-  $service = Get-Service -Name $serviceName -ErrorAction SilentlyContinue
-  if ($null -ne $service) {
-    Write-Progress -Activity "Restarting Services" -Status "Restarting $status" -PercentComplete $percentComplete
-    Restart-Service -Name $serviceName -Force:$force
+  if($null -ne (Get-Service -Name $Name -ErrorAction SilentlyContinue)) {
+    Write-Progress -Activity "Restarting Services" -Status "Restarting $Status" -PercentComplete $PercentComplete
+    Restart-Service -Name $Name -Force:$Force
   } else {
-    Write-Output "$serviceName is not found."
+    Write-Output "$Name is not found."
   }
 }
+
+$services = @(
+  @{Name = "EventSystem";PercentComplete = 10;Status = "COM+";Force = $true},
+  @{Name = "CryptSvc";PercentComplete = 20;Status = "Cryptographic";Force = $false},
+  @{Name = "Winmgmt";PercentComplete = 40;Status = "WMI";Force = $true},
+  @{Name = "SQLWriter";PercentComplete = 50;Status = "SQLWriter";Force = $false},
+  @{Name = "NTDS";PercentComplete = 60;Status = "Active Directory Domain Services";Force = $true},
+  @{Name = "VSS";PercentComplete = 80;Status = "VSS";Force = $false},
+  @{Name = "Backup Service Controller";PercentComplete = 90;Status = "Starting Backup Service Controller";Force = $false}
+)
+
 # Display progress message
 Write-Output "Restarting Services"
-Restart-ServiceWithProgress -serviceName "EventSystem" -percentComplete 10 -status "COM+" -force:$true
-Restart-ServiceWithProgress -serviceName "CryptSvc" -percentComplete 20 -status "Cryptographic"
-Restart-ServiceWithProgress -serviceName "Winmgmt" -percentComplete 40 -status "WMI" -force:$true
-Restart-ServiceWithProgress -serviceName "SQLWriter" -percentComplete 50 -status "SQLWriter"
-Restart-ServiceWithProgress -serviceName "NTDS" -percentComplete 60 -status "Active Directory Domain Services" -force:$true
-Restart-ServiceWithProgress -serviceName "VSS" -percentComplete 80 -status "VSS"
-Restart-ServiceWithProgress -serviceName "Backup Service Controller" -percentComplete 90 -status "Starting Backup Service Controller"
+
+foreach($service in $services){
+  Restart-ServiceWithProgress @service
+}
 Write-Progress -Activity "Restarting Services" -Status "Completed" -PercentComplete 100
 Write-Output "Completed"
