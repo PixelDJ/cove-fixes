8b789e16dfea8652bfec61a5392b7a187066f535
pixel <superdj@gmail.com>
2023-01-11 16:46:54 -0700

    condensed and made variables more friendly

--- Restart VSS Services.ps1
+++ Restart VSS Services.ps1
@@ -2,11 +2,11 @@ function Restart-ServiceWithProgress {
   param(
     [string]$Name,
     [int]$PercentComplete,
-    [string]$Status,
+    [string]$FriendlyName,
     [bool]$Force
   )
   if($null -ne (Get-Service -Name $Name -ErrorAction SilentlyContinue)) {
-    Write-Progress -Activity "Restarting Services" -Status "Restarting $Status" -PercentComplete $PercentComplete
+    Write-Progress -Activity "Restarting Services" -Status "Restarting $FriendlyName" -PercentComplete $PercentComplete
     Restart-Service -Name $Name -Force:$Force
   } else {
     Write-Output "$Name is not found."
@@ -14,20 +14,20 @@ function Restart-ServiceWithProgress {
 }
 
 $services = @(
-  @{Name = "EventSystem";PercentComplete = 10;Status = "COM+";Force = $true},
-  @{Name = "CryptSvc";PercentComplete = 20;Status = "Cryptographic";Force = $false},
-  @{Name = "Winmgmt";PercentComplete = 40;Status = "WMI";Force = $true},
-  @{Name = "SQLWriter";PercentComplete = 50;Status = "SQLWriter";Force = $false},
-  @{Name = "NTDS";PercentComplete = 60;Status = "Active Directory Domain Services";Force = $true},
-  @{Name = "VSS";PercentComplete = 80;Status = "VSS";Force = $false},
-  @{Name = "Backup Service Controller";PercentComplete = 90;Status = "Starting Backup Service Controller";Force = $false}
+  @{Name = "EventSystem";PercentComplete = 10;FriendlyName = "COM+";Force = $true},
+  @{Name = "CryptSvc";PercentComplete = 20;FriendlyName = "Cryptographic";Force = $false},
+  @{Name = "Winmgmt";PercentComplete = 40;FriendlyName = "WMI";Force = $true},
+  @{Name = "SQLWriter";PercentComplete = 50;FriendlyName = "SQLWriter";Force = $false},
+  @{Name = "NTDS";PercentComplete = 60;FriendlyName = "Active Directory Domain Services";Force = $true},
+  @{Name = "VSS";PercentComplete = 80;FriendlyName = "VSS";Force = $false},
+  @{Name = "Backup Service Controller";PercentComplete = 90;FriendlyName = "Backup Service Controller";Force = $false}
 )
 
-# Display progress message
 Write-Output "Restarting Services"
 
 foreach($service in $services){
   Restart-ServiceWithProgress @service
 }
+
 Write-Progress -Activity "Restarting Services" -Status "Completed" -PercentComplete 100
 Write-Output "Completed"
